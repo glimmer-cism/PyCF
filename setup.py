e3b1358132de5dfea5fbf979c65109ac4c80ba42
Magnus Hagdorn <Magnus.Hagdorn@ed.ac.uk>
2005-10-06 09:31:53 +0000

    release 0.4
    git-svn-id: https://svn.berlios.de/svnroot/repos/glimmer-cism/PyCF/trunk@559 2cfc7cc4-626f-0410-8325-f6cb4dace9bf

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ data_files = [('share/PyCF/',['data/ice.cpt',
                       'progs/plotEISMINT2stats.py'])]
 
 setup (name = "PyCF",
-       version = "0.3",
+       version = "0.4",
        description = "Python modules for CF",
        author = "Magnus Hagdorn",
        author_email = "Magnus.Hagdorn@ed.ac.uk",
